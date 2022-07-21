d002bf2f9579c26093168ba8ed25738e4009bae3
adityakhuteta <103585210+adityakhuteta@users.noreply.github.com>
2022-07-22 02:36:01 +0530

    Add files via upload

---
+++ src/requests.js
@@ -0,0 +1,25 @@
+// const APIKEY = process.env.REACTAPI
+
+// const requests={
+//     fetchTrending:`/trending/all/week?APIKEY=${APIKEY}&language=en-US`,
+//     fetchNetflixOriginals:`/discover/tv?APIKEY=${APIKEY}&with_networks=213`,
+//     fetchTopRated: `/movie/top_rated?APIKEY=${APIKEY}&language=en-US`,
+//     fetchActionMovies: `/discover/movie?APIKEY=${APIKEY}&with_genres=28`,
+//     fetchComedyMovies: `/discover/movie?APIKEY=${APIKEY}&with_genres=35`,
+//     fetchHorrorMovies: `/discover/movie?APIKEY=${APIKEY}&with_genres=27`,
+//     fetchRomanceMovies: `/discover/movie?APIKEY=${APIKEY}&with_genres=10749`,
+//     fetchDocumentaries: `/discover/movie?APIKEY=${APIKEY}&with_genres=99`,
+// }
+// export default requests;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
